bc9f90b1671d401403b49f10c581cba896373c94
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-01-06 12:34:37 +0100

    use ca-derivations

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     devshell.url = "github:numtide/devshell";
+    nixtoo.url = "github:DarkKirb/nixtoo";
+    nixtoo.flake = false;
   };
 
   outputs = inputs @ {flake-parts, ...}:
@@ -21,11 +23,20 @@
         system,
         ...
       } @ args: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (import "${inputs.nixtoo}/overlay.nix")
+            inputs.self.overlays.default
+          ];
+          config.contentAddressedByDefault = true;
+        };
+
         devshells.default.devshell.packages = with pkgs; [
           mix2nix
           elixir
         ];
-        packages.akkoma = pkgs.callPackage ./package.nix {inherit inputs;};
+        packages.akkoma = pkgs.akkoma;
         formatter = pkgs.alejandra;
         checks.akkoma = import ./test/nixpkgs.nix inputs args;
       };
@@ -34,6 +45,10 @@
           inherit (inputs.self) devShells packages formatter;
 
           inherit (inputs.self.checks) x86_64-linux;
+        };
+
+        overlays.default = self: super: {
+          akkoma = self.callPackage ./package.nix {inherit inputs;};
         };
       };
     };
